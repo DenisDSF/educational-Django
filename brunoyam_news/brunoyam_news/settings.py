fed3571133affbb00c2a9c4fd7dd44af062084e3
DenisDSF <fedoseev.ds@mail.ru>
2025-12-15 14:16:24 +0700

    chore: Внесены изменения в .gitignore. Добавлены изменения, выполненные по учебным видео модуля 11.

--- brunoyam_news/brunoyam_news/settings.py
+++ brunoyam_news/brunoyam_news/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'news'
+    'news',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
--- brunoyam_news/news/models.py
+++ brunoyam_news/news/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django.db import models
 
 class Author(models.Model):
@@ -15,3 +17,27 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Article(models.Model):
+    """
+    Статья на сайте.
+    """
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'На модерации'
+        PUBLISH = 'publish', 'Готово к публикации'
+        INACTIVE = 'inactive', 'Неактивная'
+
+    author = models.ForeignKey('users.User', related_name='articles', on_delete=models.SET_NULL, null=True, default=None)
+
+    title = models.CharField('Заголовок новости', max_length=64, unique=True)
+    description = models.CharField('Описание новости', max_length=200)
+    text = models.TextField('Текст новости')
+    status = models.CharField(max_length=8, choices=Status.choices, default=Status.DRAFT)
+
+
+    pub_date = models.DateTimeField('Дата публикации новости')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
