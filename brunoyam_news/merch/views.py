0e17332264a90c937bcb051dc1153bf4858ba858
DenisDSF <fedoseev.ds@mail.ru>
2026-01-12 15:42:07 +0700

    refactor: Присоединяем только 'category' к Product. Проверка доступности товаров вынесена в отдельную функцию. Добавлено отображение ошибки 404 к функциям products_list и product.

--- brunoyam_news/merch/views.py
+++ brunoyam_news/merch/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound
 from .models import Category, Product
 
 def merch_category(request):
@@ -8,20 +10,31 @@ def merch_category(request):
     }
     return render(request, 'category/merch_category.html', context)
 
+def checking_product_availability(all_products):
+    available_products = all_products.exclude(stock=0, supplies=False)
+    return available_products
+
 def products_list(request, category_name):
-    products = Product.objects.select_related()\
-        .filter(category__name=category_name).exclude(stock=0, supplies=False)
-    context = {
-        'category_name': category_name,
-        'products': products
-    }
-    return render(request, 'products/products_list.html', context)
+    if Category.objects.filter(name=category_name).exists():
+        all_products = Product.objects.select_related('category')\
+            .filter(category__name=category_name)
+        available_products = checking_product_availability(all_products)
+        context = {
+            'category_name': category_name,
+            'products': available_products
+        }
+        return render(request, 'products/products_list.html', context)
+    else:
+        return HttpResponseNotFound()
 
 def product(request, category_name, product_id):
     category_name = category_name
-    product = Product.objects.get(id=product_id)
-    context = {
-        'product': product,
-        'category_name': category_name
-    }
-    return render(request, 'products/product.html', context)
+    try:
+        product = Product.objects.get(id=product_id)
+        context = {
+            'product': product,
+            'category_name': category_name
+        }
+        return render(request, 'products/product.html', context)
+    except Product.DoesNotExist:
+        return HttpResponseNotFound()
